8b734c9636b72ae49c7df4826097f10ca91f3a5d
Arthur Pujols <arthurpujols@Arthurs-MacBook-Pro.local>
2018-01-12 18:53:01 -0500

    Added functionality to progress through quiz questions and check for correct answers

---
+++ Quizzler/Question.swift
@@ -0,0 +1,9 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Arthur Pujols on 1/12/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Quizzler/QuestionBank.swift
@@ -0,0 +1,9 @@
+//
+//  QuestionBank.swift
+//  Quizzler
+//
+//  Created by Arthur Pujols on 1/12/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
